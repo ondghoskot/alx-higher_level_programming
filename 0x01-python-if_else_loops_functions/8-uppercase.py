3061e4a59d09086f69f726cc1b7b08e60498e525
ondghoskot <anaschtioui65@gmail.com>
2024-01-02 20:02:46 +0000

    task done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uc = 0
+    for l in str:
+        if ord(l) >= ord('a') and ord(c) <= ord('z'):
+            uc = 32
+        else:
+            upper = 0
+        print("{:c}".format(ord(l) - uc), end='')
+    print("")
