50efb941717abe697a90b669d653615b1e11cfd4
ondghoskot <anaschtioui65@gmail.com>
2024-01-08 23:27:12 +0000

    task done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for i in range(len(my_string)):
             korok = my_string[i]
-            if korok = 'c' or korok = 'C':
+            if korok == 'c' or korok == 'C':
                 del korok
