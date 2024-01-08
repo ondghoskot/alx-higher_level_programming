f129c479c6dcf8ace57dc549057575b6eb833737
ondghoskot <anaschtioui65@gmail.com>
2024-01-08 23:38:11 +0000

    task done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return None
     new_string = ""
     for i in range(len(my_string)):
         korok = my_string[i]
-        if korok not 'c' and korok not 'C':
+        if korok not in 'cC':
             new_string += korok
     return new_string
