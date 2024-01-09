2aa13ef56e13028778f0f4d70fab280de82824de
ondghoskot <anaschtioui65@gmail.com>
2024-01-09 18:43:38 +0000

    task done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) is 0:
         return None
     imax = my_list[0]
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         if my_list[i + 1] > imax:
             imax = my_list[i + 1]
     return imax
