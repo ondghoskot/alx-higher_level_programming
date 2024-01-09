11381e222e443027b25965364e08e55ed91ba8fa
ondghoskot <anaschtioui65@gmail.com>
2024-01-09 01:53:16 +0000

    task done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     leng = len(sentence)
-    if leng is 0:
+    if sentence is None or leng is 0:
         fc = None
     fc = sentence[0]
     return (leng, fc)
