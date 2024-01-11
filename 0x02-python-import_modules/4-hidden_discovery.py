984a54d432138b245ced8d03cf5702a44ccb1871
ondghoskot <anaschtioui65@gmail.com>
2024-01-11 00:22:01 +0000

    task done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     namez = dir(hidden_4)
     i = 0
     while i < len(namez):
-        if len(namez) > 2 or namez[i][0] != '_' or namez[i][1] != '_':
+        if not (len(namez) > 2 and namez[i][0] == '_' and namez[i][1] == '_'):
             print(namez[i])
