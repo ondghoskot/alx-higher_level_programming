4c3920edfac1831df4027b4fb5118ff5b3280911
ondghoskot <anaschtioui65@gmail.com>
2024-01-11 00:09:50 +0000

    task done

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    namez = dir(hidden_4)
+    i = 0
+    while i < len(namez):
+        if len(namez) < 2 or namez[i][0] != '_' or namez[i][1] != '_':
+            print(namez[i])
