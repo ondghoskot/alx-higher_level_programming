8f23487e83970980749e4ff0ce55904a473b0df9
ondghoskot <anaschtioui65@gmail.com>
2024-02-01 03:37:42 +0000

    task done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,4 +5,4 @@
 class LockedClass:
     """class that prevents the user from dynamically creating an attribute
         unless it's called first_name"""
-        __slots__ = ["first_name"]
+    __slots__ = ["first_name"]
