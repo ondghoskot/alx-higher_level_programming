fe7791709ae4402f780b09cb16624ee4a9c384f0
ondghoskot <anaschtioui65@gmail.com>
2024-02-03 01:02:29 +0000

    task done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a function say_my_name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>
+
+        Args:
+            first_name: <first name>
+            las_name: <last_name>
+
+        Raises:
+            TypeError: if one of the args isn't a string
+
+        Returns:
+            nothing
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, las_name))
