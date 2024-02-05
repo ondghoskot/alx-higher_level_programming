adca2de00855413c78cfe3bcb96480133120fbc3
ondghoskot <anaschtioui65@gmail.com>
2024-02-05 19:43:57 +0000

    added unimplemented area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class defined with a public instance method"""
+    def area(self):
+        """public instance method that finds the area"""
+        raise Exception("area() is not implemented")
