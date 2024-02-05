af70ee306aae70a706b0209619d5747e5c14631c
ondghoskot <anaschtioui65@gmail.com>
2024-02-05 19:26:53 +0000

    created is_kind_of_class file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a inherits_frm function"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the object is an instance of a subclass of the
+    specified class
+
+    Args:
+        obj: object to check
+        a_class: class of object
+
+    Returns:
+        True if the object is an instance
+        of the subclass or False otherwise
+    """
+     return type(obj) != a_class and isinstance(obj, a_class)
