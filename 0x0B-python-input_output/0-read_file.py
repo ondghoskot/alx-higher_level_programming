dc82607cb4f1f5be92d0516faeee64c52c994c3b
ondghoskot <anaschtioui65@gmail.com>
2024-02-06 20:54:58 +0000

    added read_file function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read())
+    with open(filename,"r",  encoding="utf-8") as file1:
+        print(file1.read(), end="")
