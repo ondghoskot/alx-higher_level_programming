ed3d72553d06b93f3bc9ad59cbae150296c7642b
ondghoskot <anaschtioui65@gmail.com>
2024-02-07 10:42:22 +0000

    to_json_string function

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines function to_json_string"""
+import json
+
+def to_json_string(my_obj):
+    """returns json representation of an object"""
+    return json.dumps(my_obj)
