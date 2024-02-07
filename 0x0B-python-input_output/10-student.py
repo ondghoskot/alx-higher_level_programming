583e3760056d52712775c0e516ed250650942d0a
ondghoskot <anaschtioui65@gmail.com>
2024-02-07 17:44:59 +0000

    updated Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """class with 3 public instance attributes"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation of the public attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict representation of an instance"""
+        new_dict = {}
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list):
+            for strgs in attrs:
+                if isinstance(strgs, str) and strgs in self.__dict__:
+                    new_dict = self.__dict__[strgs]
+            return new_dict
