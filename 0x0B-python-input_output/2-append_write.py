411686c25e61030293edb2cb4aeb833372110e68
ondghoskot <anaschtioui65@gmail.com>
2024-02-07 10:35:51 +0000

    added append_write function

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """appends string to end of file and returns the numbers of characs written"""
+    """appends string to end of file and returns the numbers of chars written"""
     with open(filename, "a", encoding="utf-8") as file3:
         return file3.write(text)
