9713f9a2f9a033bab954d331fb332294a6213700
ondghoskot <anaschtioui65@gmail.com>
2024-02-07 11:25:11 +0000

    add_items script

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that adds all CLAs  to a python list and saves them to a file"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+CLA = load_from_json_file("add_item.json")
+CLA.extend(sys.argv[1:])
+save_to_json_file(CLA, "add_item.json")
