66687cf2fd3f195764895b3a02aa5a24996876b6
ondghoskot <anaschtioui65@gmail.com>
2024-02-12 19:17:56 +0000

    task done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """Subclass of Base with private instance attributes"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor to assign each arg with the right attr"""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
