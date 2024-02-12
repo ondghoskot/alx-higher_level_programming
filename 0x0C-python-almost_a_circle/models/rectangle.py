20943ef6c4b5882c8109bf3b694634bfd116c915
ondghoskot <anaschtioui65@gmail.com>
2024-02-12 19:43:12 +0000

    task done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,6 @@ class Rectangle(Base):
     def display(self):
         """prints rectangle using '#'"""
         print("\n" * self.y, end="")
-        
         for _ in range(self.height):
             print(" " * self.x, end="")
             print("#" * self.width)
