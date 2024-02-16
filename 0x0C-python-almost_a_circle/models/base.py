51226be59d98fcfac2d914fa1ab7efabf8cbca9b
ondghoskot <anaschtioui65@gmail.com>
2024-02-16 12:21:13 +0000

    task done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a class called Base"""
-
+import json
 
 class Base:
     """class with a private attr, it will be
@@ -19,3 +19,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """method that returns the json representations
+        of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
