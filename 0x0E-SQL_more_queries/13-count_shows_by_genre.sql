5a68d16ec285979d4b0b4eac70b64f30b9d18d40
ondghoskot <anaschtioui65@gmail.com>
2024-03-14 22:40:15 +0000

    DESC

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 -- the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genre.id HAVING number_of_shows > 0
+GROUP BY tv_genres.id HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
