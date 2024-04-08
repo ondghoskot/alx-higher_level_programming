29d1981174fd24875f98e8e6bb8d6991a9452189
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 15:25:42 +0000

    langs and adjs

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let langs = ['C', 'Python', 'JavaScript'];
+let adjs = ['fun', 'cool', 'amazing'];
+
+for (let i = 0;  i < langs.length && i < adjs.length; i++) {
+	console.log(langs[i] + ' is ' + adjs[i]);
+}
