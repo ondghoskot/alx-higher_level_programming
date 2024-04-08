4e21cc37bc58d51c2907d2a98b3650fd590937be
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 16:10:23 +0000

    Oooo command line args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-/*
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
-  console.log('Arguments found');
-}
-*/
 
 if (process.argv.length === 2) {
   console.log('No argument');
