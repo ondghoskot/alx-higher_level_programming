275fd2e07116681934de8db76824b2877bb2849e
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 21:37:21 +0000

    the classic add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return Number(a) + Number(b);
 }
 
 const first = process.argv[2];
 const second = process.argv[3];
 
-result = add(first, second);
+const result = add(first, second);
 console.log(result);
