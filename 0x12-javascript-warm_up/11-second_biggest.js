9e4ab3eeea3f02ed4fee0a01bde6c98c89130856
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 23:00:49 +0000

    find the Second ** max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,13 +6,13 @@ if (len > 3) {
   let SecondMax = Number(process.argv[2]);
   for (let i = 3; i < len; i++) {
     const Num = Number(process.argv[i]);
-    if (max < Num) {
+    if (!isNaN(Num) && max < Num) {
       max = Num;
     }
   }
   for (let j = 3; j < len; j++) {
     const SecondNum = Number(process.argv[j]);
-    if (SecondNum > SecondMax && SecondNum < max) {
+    if (!isNaN(SecondNum) && SecondNum > SecondMax && SecondNum < max) {
       SecondMax = SecondNum;
     }
   }
