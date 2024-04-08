531613fe86fe24a872664fbd13ff3804f12aba4d
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 22:30:11 +0000

    find the max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,13 @@
 
 const len = process.argv.length;
 if (len > 3) {
-  let max = process.argv[2];
+  let max = Number(process.argv[2]);
   for (let i = 3; i < len; i++) {
-    if (max < process.argv[i]) {
-      max = process.argv[i];
+    if (max < Number(process.argv[i])) {
+      max = Number(process.argv[i]);
     }
   }
   console.log(max);
 } else {
-  console.log('0');
+  console.log(0);
 }
