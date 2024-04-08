27549272ae50c61293b134fce8df7cfb522e565a
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 20:03:14 +0000

    semistandard js check

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,9 @@
 
 const cla = process.argv[2];
 let MyNumber = Number(cla);
-if (!isNaN(MyNumber) && typeof MyNumber === "number") {
+if (!isNaN(MyNumber) && typeof MyNumber === 'number') {
   MyNumber = Math.floor(MyNumber);
   console.log(`My number: ${MyNumber}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
