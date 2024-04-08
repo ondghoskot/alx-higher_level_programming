019dbf8762fe86a75d9d54664e6e68b4db0d9cb4
ondghoskot <anaschtioui65@gmail.com>
2024-04-08 15:29:04 +0000

    langs and adjs updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let langs = ['C', 'Python', 'JavaScript'];
-let adjs = ['fun', 'cool', 'amazing'];
+const langs = ['C', 'Python', 'JavaScript'];
+const adjs = ['fun', 'cool', 'amazing'];
 
-for (let i = 0;  i < langs.length && i < adjs.length; i++) {
-	console.log(langs[i] + ' is ' + adjs[i]);
+for (let i = 0; i < langs.length && i < adjs.length; i++) {
+  console.log(langs[i] + ' is ' + adjs[i]);
 }
