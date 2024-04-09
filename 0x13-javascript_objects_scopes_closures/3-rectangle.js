834c84821c873574cc63da609e9294f7abe679ea
ondghoskot <anaschtioui65@gmail.com>
2024-04-09 20:48:31 +0000

    print()

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.heigth; i++) {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
         row += 'X';
