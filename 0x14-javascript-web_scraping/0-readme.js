11e2b42ef7457be9199d988b7515a091682c4cf6
anassch <anaschtioui65@gmail.com>
2024-05-21 22:29:19 +0000

    web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (error, data) => {
-	if (error) {
-		return console.log(error);
-	} else {
-		console.log(data);
-	}
+  if (error) {
+    return console.log(error);
+  } else {
+    console.log(data);
+  }
 });
